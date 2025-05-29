6f4753e9cc81413aa68fa342905d2771df03297a
Aaron Shannon <ayren.shannon@me.com>
2025-05-29 01:16:50 -0700

    removed unused scroll js

--- js/scroll.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function () {
-    const scrollIndicator = document.querySelector('.scroll-down');
-    if (window.scrollY > 100) {
-      scrollIndicator.classList.add('hide');
-    } else {
-      scrollIndicator.classList.remove('hide');
-    }
-  });
